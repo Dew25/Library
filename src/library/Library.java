80dc8e51f238d9df50da7a22a4d8093fae18afb1
jvm <jvm@debian-home.jvm.ee>
2018-09-07 00:10:19 +0300

    Шаг 1. Выдача книги читателю

---
+++ src/library/Library.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import entity.Book;
+import entity.LibHistory;
+import entity.Reader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author jvm
+ */
+public class Library {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Book book1 = new Book(1L, "Voina i Mir", "123-123123", "Lev Tolstoy", 2010);
+       Reader reader1 = new Reader(1L, "Ivan", "Ivanov", "56565656", "Johvi");
+       
+       Calendar c = new GregorianCalendar();
+      
+       LibHistory libHistory1 = new LibHistory(1L, book1, reader1, c.getTime(), null);
+       
+        System.out.println(book1.toString());
+        System.out.println(reader1.toString());
+        System.out.println(libHistory1.toString());
+        
+    }
+    
+}
